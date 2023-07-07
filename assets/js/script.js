10ab65971eacb4ade098af62381794b6f763d4fa
Tue-GBS <mateusdasilva72@outlok.com>
2023-07-07 14:38:33 -0300

    V3.9

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 function menuShow () {
-    let menuMobile = document.querySelector('.mobile')
+    let menuMobile = document.querySelector('.mobile');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
+        document.querySelector('.icon').scr= "assets\img\icons8-menu-100.png"
     }
     else {
         menuMobile.classList.add('open')
+        document.querySelector('.icon').scr= "assets\img\icons8-close-window-96.png"
     }
 }
